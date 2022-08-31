801b9df250003084bb6280c6f1abf94899004856
Administrator <Administrator@User -2022TSFVF>
2022-08-31 12:18:02 +0900

    /board/list /board/register 한글처리

--- jex02/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ jex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -17,7 +17,7 @@ import lombok.extern.log4j.Log4j;
 public class BoardServiceImpl implements BoardService {
 	//spring 4.3 이상에서 자동 처리
 	@Setter(onMethod_ = @Autowired)
-	public BoardMapper mapper;
+	private BoardMapper mapper;
 	
 	//등록작업
 	@Override
